8612e107a7a52a4f408c9f090682c18ae8a2a4a8
tristen512 <tristenspare@gmail.com>
2026-01-05 19:22:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function showHero() {
     if (hero === "") {
         heroName.textContent = "Choose a Hero";
         heroPower.textContent = "Select a hero to see their abilities.";
-        heroImage.src = "images/hero-default.png";
+        heroImage.src = "hero-default.png";
         return;
     }
 
@@ -18,27 +18,27 @@ function showHero() {
         flash: {
             name: "The Flash",
             power: "Super speed, time travel, lightning strikes.",
-            image: "images/flash.png"
+            image: "flash.png"
         },
         thor: {
             name: "Thor",
             power: "God of Thunder, lightning control, Mjölnir hammer.",
-            image: "images/thor.png"
+            image: "thor.png"
         },
         ironman: {
             name: "Iron Man",
             power: "Genius billionaire, high-tech suits, missiles & lasers.",
-            image: "images/ironman.png"
+            image: "ironman.png"
         },
         wanda: {
             name: "Scarlet Witch",
             power: "Chaos magic, telekinesis, reality manipulation.",
-            image: "images/wanda.png"
+            image: "wanda.png"
         },
         spiderman: {
             name: "Spider-Man",
             power: "Wall-crawling, web-shooting, spider sense.",
-            image: "images/spiderman.png"
+            image: "spiderman.png"
         }
     };
 
